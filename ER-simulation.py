5585c498c2b5a4ab19c94242d99f11a38963fe32
Akirooo <87535218+Akirooo@users.noreply.github.com>
2023-11-16 21:48:03 +0800

    Update ER-simulation.py

--- ER-simulation.py
+++ ER-simulation.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import function_general
 import math
+import multiprocessing
 
 
-def m_index(g,g_stat,i):
+def m_index(g, g_stat, i):
     m = 0
     j_list = list(g.predecessors(i))
     k_list = []
@@ -15,30 +16,32 @@ def m_index(g,g_stat,i):
         if g_stat[j] == 1:
             k_list = list(g.predecessors(j))
             for k in k_list:
-                if g_stat[k]==1:
+                if g_stat[k] == 1:
                     tmp_k += 1
         m_list.append(tmp_k)
-    if len(m_list)> 0:
+    if len(m_list) > 0:
         m = max(m_list)
     else:
         m = 0
     return int(m)
 
+
 def g_active_num(g_stat):
     num = 0
-    for (v,s) in g_stat.items():
-        if s ==1:
-            num +=1
+    for (v, s) in g_stat.items():
+        if s == 1:
+            num += 1
     return num
 
-def test_m_index(g,g_stat):
+
+def test_m_index(g, g_stat):
     for v in g.nodes():
-        mm = m_index(g,g_stat,v)
+        mm = m_index(g, g_stat, v)
         print(str(mm))
-    node_labels = dict([(v,g_stat[v]) for v in g.nodes()])
+    node_labels = dict([(v, g_stat[v]) for v in g.nodes()])
     position = nx.circular_layout(g)
-    nx.draw_networkx_labels(g,pos=position,labels=node_labels)
-    nx.draw_networkx(g,pos=position,with_labels=False)
+    nx.draw_networkx_labels(g, pos=position, labels=node_labels)
+    nx.draw_networkx(g, pos=position, with_labels=False)
     plt.show()
     return
 
@@ -50,11 +53,12 @@ def gen_possion_distribution(ave_k, k_max):
         p_k[kk] = p_k[kk - 1] * ave_k / kk
     return p_k
 
+
 def simulation(m, k):
     ##变量区
-    n = 100000
-    p = k/n
-    g = nx.erdos_renyi_graph(n, p, directed=True)
+    n = 1000000
+    p = k / n
+    g = nx.fast_gnp_random_graph(n, p, directed=True)
 
     ## 状态初始化
     g_stat = {}
@@ -67,7 +71,7 @@ def simulation(m, k):
         num1 = g_active_num(g_stat)
         for v in range(0, len(g)):
             if g_stat[v] == 1:
-                v_m = m_index(g,g_stat,v)
+                v_m = m_index(g, g_stat, v)
                 if v_m < m:
                     g_stat[v] = 0
         num2 = g_active_num(g_stat)
@@ -81,34 +85,53 @@ def simulation(m, k):
             g.remove_node(v)
 
     g_list = sorted(list(nx.strongly_connected_components(g)), key=len, reverse=True)
+    if not g_list:  # 检查 g_list 是否非空
+        return 0, A
+
     gSCC = list(g_list[0])
+    if not gSCC:
+        return 0, A
+
     gout_list = list(nx.descendants(g, source=gSCC[0]))
-    GOUT = len(gout_list)/n
+    GOUT = len(gout_list) / n
 
     return GOUT, A
 
-def test_simulation():
-    for m in range(1, 7):
-        index_f = str(m) + '.txt'
-        out_file_1 = open('data/simu_' + index_f, 'w')
+
+# 新增一个函数用于每个进程
+def run_simulation_for_m(m):
+    index_f = str(m) + '.txt'
+    out_file_1 = open('data/simu_' + index_f, 'w')
+    l_GOUT = []
+    for average_k in np.arange(0, 10.00001, 0.1):
         l_GOUT = []
-        for average_k in np.arange(1, 10.00001, 0.1):
-            l_GOUT = []
-            for i in range(1):
-                print("k:" + str(average_k))
-                GOUT = simulation(m, average_k)[0]
-                print("GOUT:" + str(GOUT))
-                l_GOUT.append(GOUT)
-            sum = 0
-            for ii in l_GOUT:
-                sum += ii
-            GOUT_AVG = sum / 1
-            print("average_k:" + str(average_k))
-            print("GOUT:" + str(GOUT_AVG))
-            if average_k < 10:
-                out_file_1.write('%f\t%f\n' % (average_k, GOUT_AVG))
-            else:
-                out_file_1.write('%f\t%f' % (average_k, GOUT_AVG))
+        for i in range(1):
+            print("k:" + str(average_k))
+            GOUT = simulation(m, average_k)[0]
+            print("GOUT:" + str(GOUT))
+            l_GOUT.append(GOUT)
+        sum = 0
+        for ii in l_GOUT:
+            sum += ii
+        GOUT_AVG = sum / 1
+        print("average_k:" + str(average_k))
+        print("GOUT:" + str(GOUT_AVG))
+        if average_k < 10:
+            out_file_1.write('%f\t%f\n' % (average_k, GOUT_AVG))
+        else:
+            out_file_1.write('%f\t%f' % (average_k, GOUT_AVG))
+
+
+def test_simulation():
+    processes = []
+    for m in range(1, 7):
+        p = multiprocessing.Process(target=run_simulation_for_m, args=(m,))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
 
 if __name__ == "__main__":
     test_simulation()
